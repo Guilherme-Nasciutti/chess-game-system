cc07aa8e2856e58ee2a42a9588fee0715918041d
Guilherme-Nasciutti <guilhermenasciutti@hotmail.com>
2023-07-10 12:09:13 -0300

    Implementando possíveis movimentos da Torre // Implementing possible moves of Rook

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
 
@@ -14,5 +15,11 @@ public abstract class ChessPiece extends Piece {
 
     public Color getColor() {
         return color;
+    }
+
+    /*Função para descobrir se a peça é adversária na posição desejada*/
+    protected boolean isThereOpponentPiece(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p != null && p.getColor() != color;
     }
 }
